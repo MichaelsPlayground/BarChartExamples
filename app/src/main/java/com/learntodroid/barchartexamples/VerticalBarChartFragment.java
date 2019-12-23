89a886e0dcd1cc8d5de731dde0a9ad82f8cf3ede
learntodroid <56079350+learntodroid@users.noreply.github.com>
2019-12-23 19:35:29 +1100

    clean up vertical bar chart fragment

--- app/src/main/java/com/learntodroid/barchartexamples/VerticalBarChartFragment.java
+++ app/src/main/java/com/learntodroid/barchartexamples/VerticalBarChartFragment.java
@@ -8,9 +8,12 @@ import android.view.ViewGroup;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.Chart;
 import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 
 import java.util.ArrayList;
@@ -20,11 +23,13 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class VerticalBarChartFragment extends Fragment {
-    private static final int MAX_X_VALUE = 30;
+    private static final int MAX_X_VALUE = 7;
     private static final int MAX_Y_VALUE = 50;
     private static final int MIN_Y_VALUE = 5;
-    private static final String SET_LABEL = "Things Set";
-    BarChart chart;
+    private static final String SET_LABEL = "App Downloads";
+    private static final String[] DAYS = { "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" };
+
+    private BarChart chart;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,12 +43,40 @@ public class VerticalBarChartFragment extends Fragment {
 
         chart = view.findViewById(R.id.fragment_verticalbarchart_chart);
 
+        BarData data = createChartData();
+        configureChartAppearance();
+        prepareChartData(data);
+
+        return view;
+    }
+
+    private void configureChartAppearance() {
+        chart.getDescription().setEnabled(false);
+        chart.setDrawValueAboveBar(false);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getFormattedValue(float value) {
+                return DAYS[(int) value];
+            }
+        });
+
+        YAxis axisLeft = chart.getAxisLeft();
+        axisLeft.setGranularity(10f);
+
+        YAxis axisRight = chart.getAxisRight();
+        axisRight.setGranularity(10f);
+    }
+
+    private BarData createChartData() {
         ArrayList<BarEntry> values = new ArrayList<>();
         for (int i = 0; i < MAX_X_VALUE; i++) {
             float x = i;
-            float y = Math.max(MIN_Y_VALUE, (float) Math.random() * (MAX_Y_VALUE + 1));
+            float y = new Util().randomFloatBetween(MIN_Y_VALUE, MAX_Y_VALUE);
             values.add(new BarEntry(x, y));
         }
+
         BarDataSet set1 = new BarDataSet(values, SET_LABEL);
 
         ArrayList<IBarDataSet> dataSets = new ArrayList<>();
@@ -51,12 +84,11 @@ public class VerticalBarChartFragment extends Fragment {
 
         BarData data = new BarData(dataSets);
 
-        Description d = new Description();
-        d.setText("Vertical Bar Chart");
-        chart.setDescription(d);
+        return data;
+    }
 
+    private void prepareChartData(BarData data) {
+        data.setValueTextSize(12f);
         chart.setData(data);
-
-        return view;
     }
 }
